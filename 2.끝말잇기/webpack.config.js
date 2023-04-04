8fc8649754780b0d020fdf1d3db0470ceb467eee
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-04-05 06:12:22 +0900

    모듈 시스템과 웹팩 설정

--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  name: "word-relay-setting",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  }, // entry 에 있는 파일 확장자를 찾기 위해 resolve extensions에 명시하면 알아서 찾음
+  entry: {
+    app: ["./client"],
+  }, // 입력
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, // 출력
+};
