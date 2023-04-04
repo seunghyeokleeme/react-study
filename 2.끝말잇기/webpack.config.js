b2820963838fd5bdda25cb325644011dc4293814
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-04-05 06:38:55 +0900

    웹팩으로 빌드하기

--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -10,6 +10,17 @@ module.exports = {
   entry: {
     app: ["./client"],
   }, // 입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
