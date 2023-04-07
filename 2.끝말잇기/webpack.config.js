e2b529c725b28bd40ab0a991fd2d578d3b116372
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-04-07 14:57:23 +0900

    @babel/preset-env와 plugins

--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   name: "word-relay-setting",
@@ -7,6 +8,7 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   }, // entry 에 있는 파일 확장자를 찾기 위해 resolve extensions에 명시하면 알아서 찾음
+
   entry: {
     app: ["./client"],
   }, // 입력
@@ -16,11 +18,24 @@ module.exports = {
         test: /\.jsx?/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR", "last 2 chrome versions"],
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [],
         },
       },
     ],
   },
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
