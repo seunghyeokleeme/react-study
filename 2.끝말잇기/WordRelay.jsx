725be07662153f3ba498973b547dac0b17094b32
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-04-07 15:24:24 +0900

    끝말잇기 Class 만들기

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -3,11 +3,47 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: "Hello, webpack",
+    word: "이승혁",
+    value: "",
+    result: "",
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({ result: "딩동댕동", word: this.state.value, value: "" });
+      this.input.focus();
+    } else {
+      this.setState({ result: "땡", value: "" });
+      this.input.focus();
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
   };
 
   render() {
-    return <h1>{this.state.text}</h1>;
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            ref={this.onRefInput}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button type="submit">입력</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
